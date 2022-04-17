1d9456ccb0217a20facb94714e2f1bef5ff4a1d9
Vladimir Loginov <BoJIodya@yandex.ru>
2022-04-17 17:56:51 +0300

    решил задачу № 2

--- Задача2/Program.cs
+++ Задача2/Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int a = 11;
+int b = 2;
+int c = 3;
+
+int max = a;
+
+if (b > max)
+{
+    max = b;
+}
+if (c > max)
+{
+    max = c;
+}
+Console.WriteLine(max);
