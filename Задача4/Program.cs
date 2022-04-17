93aabed600ec1c681379578c66a70facb0fb938c
Vladimir Loginov <BoJIodya@yandex.ru>
2022-04-17 19:05:53 +0300

    решение задачи 4

--- Задача4/Program.cs
+++ Задача4/Program.cs
@@ -1,2 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int N = 10;
+int a = -N;
+
+while (a < N + 1)
+{
+if(a%2==0)
+{
+    Console.Write(a);
+    Console.Write(", ");
+}
+a++;
+}
