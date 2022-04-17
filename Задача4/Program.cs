c4c6ae02236f6faf5b9553853f1bd6c632a23ba5
Vladimir Loginov <BoJIodya@yandex.ru>
2022-04-17 19:07:49 +0300

    форматирование кода

--- Задача4/Program.cs
+++ Задача4/Program.cs
@@ -3,10 +3,10 @@ int a = -N;
 
 while (a < N + 1)
 {
-if(a%2==0)
-{
-    Console.Write(a);
-    Console.Write(", ");
-}
-a++;
+    if (a % 2 == 0)
+    {
+        Console.Write(a);
+        Console.Write(", ");
+    }
+    a++;
 }
