6d5df3df28fbf01f96a691742257ca07aab77a61
Vladimir Loginov <BoJIodya@yandex.ru>
2022-04-17 18:52:49 +0300

    Форматирование Program3

--- Задача3/Program.cs
+++ Задача3/Program.cs
@@ -1,12 +1,12 @@
 ﻿int a = 55;
 
-if(a%2==0)
+if (a % 2 == 0)
 {
     Console.Write(a);
-    Console.WriteLine (" чётное");
+    Console.WriteLine(" чётное");
 }
 else
 {
-   Console.Write(a);
-   Console.WriteLine (" нечётное"); 
+    Console.Write(a);
+    Console.WriteLine(" нечётное");
 }
